dd5de01b124b444541268e38e24fefd28d0c1f66
matz <>
1999-09-18 04:48:51 +0000

    990918-repack

--- ruby/sample/goodfriday.rb
+++ ruby/sample/goodfriday.rb
@@ -4,9 +4,22 @@
 # $Id$
 
 require 'date2'
-require 'holiday'
 
-es = Date.easter(Date.today.year)
+def easter(y)
+  g = (y % 19) + 1
+  c = (y / 100) + 1
+  x = (3 * c / 4) - 12
+  z = ((8 * c + 5) / 25) - 5
+  d = (5 * y / 4) - x - 10
+  e = (11 * g + 20 + z - x) % 30
+  e += 1 if e == 25 and g > 11 or e == 24
+  n = 44 - e
+  n += 30 if n < 21
+  n = n + 7 - ((d + n) % 7)
+  if n <= 31 then [y, 3, n] else [y, 4, n - 31] end
+end
+
+es = Date.new3(*easter(Time.now.year))
 [[-9*7, 'Septuagesima Sunday'],
  [-8*7, 'Sexagesima Sunday'],
  [-7*7, 'Quinquagesima Sunday (Shrove Sunday)'],
