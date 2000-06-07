d7b11d01637b4c516983c97b895455132181d637
andy <>
2000-06-07 15:37:26 +0000

    New file

--- rubicon/builtin/TestThreadGroup.rb
+++ rubicon/builtin/TestThreadGroup.rb
@@ -3,16 +3,30 @@ require '../rubicon'
 
 class TestThreadGroup < Rubicon::TestCase
 
+  def test_00sanity
+    tg = ThreadGroup::Default
+    assert_equal( Thread.current, tg.list[0])
+  end
+
   def test_add
-    assert_fail("untested")
+    tg = ThreadGroup.new
+    tg.add(Thread.current)
+    assert_equal(1, tg.list.length)
+    assert_equal(0, ThreadGroup::Default.list.length)
   end
 
   def test_list
-    assert_fail("untested")
+    tg = ThreadGroup.new
+    10.times {
+      t = Thread.new { sleep 60 }
+      tg.add(t)
+    }
+    assert_equals(tg.list.length,10)
   end
 
   def test_s_new
-    assert_fail("untested")
+    tg = ThreadGroup.new
+    assert_equal(0, tg.list.length)
   end
 
 end
