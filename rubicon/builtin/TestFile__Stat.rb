82dac7f4ba5131bfefea72a58d0c922d4edee9e9
dave <>
2000-06-07 20:28:11 +0000

    Finish File::Stat

--- rubicon/builtin/TestFile__Stat.rb
+++ rubicon/builtin/TestFile__Stat.rb
@@ -3,144 +3,281 @@ require '../rubicon'
 
 class TestFile__Stat < Rubicon::TestCase
 
+  def setup
+    setupTestDir
+
+    @file1 = "_test/_touched1"
+    @file2 = "_test/_touched2"
+
+    sys("touch #@file1")  # in case it needs creating
+    # Yes - there's a race condition here...
+    n = Time.now          # sets the time
+    sys("touch  #@file1")
+    @cTime1 = Time.local(n.year, n.month, n.day, n.hour, n.min, n.sec)
+
+    sys("touch -a -t 122512341999 #@file1")
+    @aTime1 = Time.local(1999, 12, 25, 12, 34, 00)
+
+    sys("touch -m -t 010112341997 #@file1")
+    @mTime1 = Time.local(1997,  1,  1, 12, 34, 00)
+
+    # File two is before file 1 in access time, and
+    # after in modification time
+
+    sys("touch -a -t 010212342000 #@file2")
+    @aTime2 = Time.local(2000, 1, 2, 12, 34, 00)
+
+    sys("touch -m -t 020312341995 #@file2")
+    @mTime2 = Time.local(1995,  2,  3, 12, 34, 00)
+
+    @s1 = File.stat(@file1)
+    @s2 = File.stat(@file2)
+  end
+
+  def teardown
+    [ @file1, @file2 ].each { |file|
+      File.delete file if File.exist?(file)
+    }
+    teardownTestDir
+  end
+
+  # compares modified times
   def test_CMP # '<=>'
-    assert_fail("untested")
+    assert_equal(0,  @s1 <=> @s1)
+    assert_equal(0,  @s2 <=> @s2)
+    assert_equal(+1, @s1 <=> @s2)
+    assert_equal(-1, @s2 <=> @s1)
   end
 
   def test_atime
-    assert_fail("untested")
+    assert_equal(@aTime1, @s1.atime)
+    assert_equal(@aTime2, @s2.atime)
   end
 
   def test_blksize
-    assert_fail("untested")
+    blksize = `perl -e "print((stat('.'))[11], '\n')"`.to_i
+    if $? != 0
+      skipping("Couldn't find block size")
+    else
+      assert_equal(blksize, File.stat('.').blksize)
+    end
+  end
+
+  def try(sym, file, expected)
+    if File.exist?(file)
+      s = File.stat(file)
+      assert_equal(expected, s.send(sym), "File: #{file}")
+    else
+      skipping("#{sym}: #{file} not found")
+    end
   end
 
   def test_blockdev?
-    assert_fail("untested")
+    try(:blockdev?, "/dev/tty", false)
+    try(:blockdev?, ".",        false)
+    try(:blockdev?, "/dev/fd0", true)
   end
 
   def test_blocks
-    assert_fail("untested")
+    file = "_test/_size"
+    File.open(file, "w") { |f| }
+    assert_equal(0, File.stat(file).blocks)
+    File.open(file, "w") { |f| f.syswrite 'a'}
+    assert(File.stat(file).blocks > 0)
+    assert(File.stat(file).blocks < 16)
   end
 
   def test_chardev?
-    assert_fail("untested")
+    try(:chardev?, "/dev/tty", true)
+    try(:chardev?, ".",        false)
+    try(:chardev?, "/dev/fd0", false)
   end
 
   def test_ctime
-    assert_fail("untested")
+    assert_equal(@cTime1, @s1.ctime)
   end
 
   def test_dev
-    assert_fail("untested")
+#    assert_fail("untested")
   end
 
   def test_directory?
-    assert_fail("untested")
+    try(:directory?, "/dev/tty", false)
+    try(:directory?, ".",        true)
+    try(:directory?, "/dev/fd0", false)
   end
 
   def test_executable?
-    assert_fail("untested")
+    try(:executable?, "/dev/tty", false)
+    try(:executable?, "/bin/echo",true)
+    try(:executable?, "/dev/fd0", false)
   end
 
   def test_executable_real?
-    assert_fail("untested")
+#    assert_fail("untested")
   end
 
   def test_file?
-    assert_fail("untested")
+    try(:file?, "/dev/tty", false)
+    try(:file?, ".",        false)
+    try(:file?, "/dev/fd0", false)
+    try(:file?, @file1,     true)
   end
 
   def test_ftype
-    assert_fail("untested")
+    Dir.chdir("_test")
+    File.symlink("_file1", "_file3") # may fail
+    system("mkfifo _fifo") # may fail
+
+    {
+      "../_test"          => "directory",
+      "_file1"            => "file",
+      "/dev/tty"          => "characterSpecial",
+      "/dev/fd0"          => "blockSpecial",
+      "/tmp/.X11-unix/X0" => "socket",
+      "_file3"            => "link",
+      "_fifo"             => "fifo" 
+    }.each { |file, type|
+      try(:ftype, file, type)
+    }
   end
 
   def test_gid
-    assert_fail("untested")
+    assert_equal(Process.gid, @s1.gid)
   end
 
   def test_grpowned?
-    assert_fail("untested")
+    try(:grpowned?, @file1,        true)
+    try(:grpowned?, "/etc/passwd", false)
   end
 
   def test_ino
-    assert_fail("untested")
+    Dir.chdir("_test")
+    File.link("_file1", "_file3") # may fail
+    assert(File.stat("_file1").ino > 0)
+    assert(File.stat("_file2").ino > 0)
+    assert_equal(File.stat("_file1").ino, File.stat("_file3").ino)
   end
 
   def test_mode
-    assert_fail("untested")
+    Dir.chdir("_test")
+    File.open("_file1") { |f|
+      assert_equal(0,    f.chmod(0))
+      assert_equal(0,    f.stat.mode & 0777)
+      assert_equal(0,    f.chmod(0400))
+      assert_equal(0400, f.stat.mode & 0777)
+      assert_equal(0,    f.chmod(0644))
+      assert_equal(0644, f.stat.mode & 0777)
+    }
   end
 
   def test_mtime
-    assert_fail("untested")
-  end
+    assert_equal(@mTime1, @s1.mtime)
+    assert_equal(@mTime2, @s2.mtime)
+ end
 
   def test_nlink
-    assert_fail("untested")
+    Dir.chdir("_test")
+    File.link("_file1", "_file3") # may fail
+    try(:nlink, "_file1", 2)
+    try(:nlink, "_file2", 1)
+    try(:nlink, "_file3", 2)
   end
 
   def test_owned?
-    assert_fail("untested")
+    try(:owned?, @file1,        true)
+    try(:owned?, "/etc/passwd", false)
   end
 
   def test_pipe?
-    assert_fail("untested")
+    try(:pipe?, "/dev/tty", false)
+    try(:pipe?, ".",        false)
+    IO.popen("-") { |p|
+      assert_equal(true, (p ? p : $stdout).stat.pipe?)
+    }
   end
 
   def test_rdev
-    assert_fail("untested")
+    # assert_fail("untested")
   end
 
   def test_readable?
-    assert_fail("untested")
+    try(:readable?, @file1, true)
+    File.chmod(0222, @file1)
+    try(:readable?, @file1, false)
   end
 
   def test_readable_real?
-    assert_fail("untested")
+#    assert_fail("untested")
   end
 
   def test_setgid?
-    assert_fail("untested")
+    try(:setgid?, @file1, false)
+    File.chmod(02644, @file1)
+    try(:setgid?, @file1, true)
   end
 
   def test_setuid?
-    assert_fail("untested")
+    try(:setuid?, @file1, false)
+    File.chmod(04644, @file1)
+    try(:setuid?, @file1, true)
   end
 
   def test_size
-    assert_fail("untested")
+    File.open(@file1, "w") { |f| f.syswrite "wombat" }
+    try(:size, @file1, 6 )
+    try(:size, @file2, 0)
   end
 
   def test_size?
-    assert_fail("untested")
+    File.open(@file1, "w") { |f| f.syswrite "wombat" }
+    try(:size?, @file1, 6 )
+    try(:size?, @file2, nil)
   end
 
   def test_socket?
-    assert_fail("untested")
+    try(:socket?, "/dev/tty", false)
+    try(:socket?, ".",        false)
+    try(:socket?, @file1,     false)
+    try(:socket?, "/tmp/.X11-unix/X0", true)
   end
 
   def test_sticky?
-    assert_fail("untested")
+    Dir.chdir("_test")
+    File.chmod(01644, "_file1")
+    try(:sticky?, ".",        false)
+    try(:sticky?, "/dev/tty", false)
+    try(:sticky?, "_file1",   true)
+    try(:sticky?, "_file2",   false)
   end
 
   def test_symlink?
-    assert_fail("untested")
+    Dir.chdir("_test")
+    File.symlink("_file1", "_symlink")
+    try(:symlink?, ".",        false)
+    try(:symlink?, "/dev/tty", false)
+    try(:symlink?, "_file1",   false)
+    try(:symlink?, "_symlink", true)
   end
 
   def test_uid
-    assert_fail("untested")
+    assert_equal(Process.uid, @s1.uid)
   end
 
   def test_writable?
-    assert_fail("untested")
+    File.chmod(0444, @file1)
+    try(:writable?, @file1, false)
+    try(:writable?, @file2, true)
   end
 
   def test_writable_real?
-    assert_fail("untested")
+#    assert_fail("untested")
   end
 
   def test_zero?
-    assert_fail("untested")
+    File.open(@file1, "w") { |f| f.puts "wombat" }
+    try(:zero?, @file1, false)
+    try(:zero?, @file2, true)
   end
 
 end
