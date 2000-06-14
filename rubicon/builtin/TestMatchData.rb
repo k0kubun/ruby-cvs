4a690f701a0a1b7d638c83c780367dfff42bd22e
dave <>
2000-06-14 03:20:42 +0000

    Added Method

--- rubicon/builtin/TestMatchData.rb
+++ rubicon/builtin/TestMatchData.rb
@@ -5,14 +5,13 @@ class TestMatchData < Rubicon::TestCase
 
   def setup
     @m = /(.)(.)(\d+)(\d)/.match("THX1138.")
-    end
-
+  end
+  
   def test_AREF # '[]'
     assert_equal("HX1138",      @m[0])
     assert_equal(%w( H X ),     @m[1, 2])
     assert_equal(%w( H X 113 ), @m[1..3])
     assert_equal(%w( X 113 ),   @m[-3, 2])
-
   end
 
   def test_begin
--- rubicon/builtin/TestMethod.rb
+++ rubicon/builtin/TestMethod.rb
@@ -3,32 +3,35 @@ require '../rubicon'
 
 class TestMethod < Rubicon::TestCase
 
+  def setup
+    @m1 = 12.method("+")
+  end
+
   def test_AREF # '[]'
-    assert_fail("untested")
+    assert_equal(15, @m1[3])
+    assert_equal(9,  @m1[-3])
+  end
+
+  def dummy1(a, b=0, c=1)
   end
 
   def test_arity
-    assert_fail("untested")
+    assert_equal(1, @m1.arity)
+    assert_equal(0, self.method("test_arity").arity)
+    assert_equal(-2, self.method("dummy1").arity)
+    assert_equal(-1, @m1.method("call").arity)
+    assert_equal(-1, @m1.method("respond_to?").arity)
   end
 
   def test_call
-    assert_fail("untested")
-  end
-
-  def test_clone
-    assert_fail("untested")
-  end
-
-  def test_inspect
-    assert_fail("untested")
+    assert_equal(15, @m1.call(3))
+    assert_equal(9,  @m1.call(-3))
   end
 
   def test_to_proc
-    assert_fail("untested")
-  end
-
-  def test_to_s
-    assert_fail("untested")
+    p = @m1.to_proc
+    assert_instance_of(p, Proc)
+    assert_equal(15, p.call(3))
   end
 
 end
