9f5dea04d52c1e92d20412292a39936e585f3e2e
andy <>
2000-06-14 20:42:15 +0000

    done.

--- rubicon/builtin/TestRange.rb
+++ rubicon/builtin/TestRange.rb
@@ -4,47 +4,108 @@ require '../rubicon'
 class TestRange < Rubicon::TestCase
 
   def test_VERY_EQUAL # '==='
-    assert_fail("untested")
+    gotit = false
+    case 52
+      when 0..49
+        assert_fail("Shouldn't have matched")
+      when 50..75
+        gotit = true
+      else
+        assert_fail("Shouldn't have matched")
+    end
+    assert_equal(true,gotit)
+
+    gotit = false
+    case 50
+      when 0..49
+        assert_fail("Shouldn't have matched")
+      when 50..75
+        gotit = true
+      else
+        assert_fail("Shouldn't have matched")
+    end
+    assert_equal(true,gotit)
+
+    gotit = false
+    case 75
+      when 0..49
+        assert_fail("Shouldn't have matched")
+      when 50..75
+        gotit = true
+      else
+        assert_fail("Shouldn't have matched")
+    end
+    assert_equal(true,gotit)
   end
 
   def test_begin
-    assert_fail("untested")
+    assert_equal(1, (1..10).begin)
+    assert_equal("a", ("a".."z").begin)
+    assert_equal(1, (1...10).begin)
+    assert_equal("a", ("a"..."z").begin)
   end
 
   def test_each
-    assert_fail("untested")
+    index = 1
+    count = 0
+    (1..10).each {|x| assert_equal(index, x)
+      index += 1
+      count += 1 }
+    assert_equal(10,count)
+
+    index = "A"
+    count = 0
+    ("A".."J").each {|x| assert_equal(index, x)
+      index.succ!
+      count += 1 }
+    assert_equal(10,count)
+
   end
 
   def test_end
-    assert_fail("untested")
+    assert_equal(10, (1..10).end)
+    assert_equal(11, (1...11).end)
+    assert_equal("z", ("a".."z").end)
+    assert_equal("A", ("a"..."A").end)
   end
 
   def test_exclude_end?
-    assert_fail("untested")
+    assert_equal(true, (1...10).exclude_end?)
+    assert_equal(false,(1..10).exclude_end?)
+    assert_equal(true, ("A"..."Z").exclude_end?)
+    assert_equal(false,("A".."Z").exclude_end?)
   end
 
   def test_first
-    assert_fail("untested")
-  end
-
-  def test_inspect
-    assert_fail("untested")
+    assert_equal(1, (1..10).first)
+    assert_equal("a", ("a".."z").first)
+    assert_equal(1, (1...10).first)
+    assert_equal("a", ("a"..."z").first)
   end
 
   def test_last
-    assert_fail("untested")
+    assert_equal(10, (1..10).last)
+    assert_equal(11, (1...11).last)
+    assert_equal("z", ("a".."z").last)
+    assert_equal("A", ("a"..."A").last)
   end
 
   def test_length
-    assert_fail("untested")
+    assert_equal(10, (1..10).length)
+    assert_equal(10, (1...11).length)
+    assert_equal(1000, (1..1000).length)
+    assert_equal(26, ("A".."Z").length)
   end
 
   def test_size
-    assert_fail("untested")
+    assert_equal(10, (1..10).length)
+    assert_equal(10, (1...11).length)
+    assert_equal(1000, (1..1000).length)
+    assert_equal(26, ("A".."Z").length)
   end
 
   def test_to_s
-    assert_fail("untested")
+    assert_equal("1..10",(1..10).to_s)
   end
 
 end
