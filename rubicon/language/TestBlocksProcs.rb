43e7f9442ba3eaf1185499c7b3864156748ba6df
dave <>
2000-06-30 21:14:10 +0000

    Initial load of files

---
+++ rubicon/language/TestBlocksProcs.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# $Id$
+#
+# This file is part of Rubicon, a set of regression tests for the Ruby
+# language and its built-in classes and modules.
+#
+# Initial development by Dave Thomas and Andy Hunt.
+#
+# Copyright (c) 2000 The Pragmatic Programmers, LLC (www.pragmaticprogrammer.com)
+# Distributed according to the terms specified in the Ruby distribution README file.
+#
+
+require '../rubicon'
+
+class TestBlocksProcs < Rubicon::TestCase
+
+end
+
+# Run these tests if invoked directly
+
+Rubicon::handleTests(TestBlocksProcs) if $0 == __FILE__
