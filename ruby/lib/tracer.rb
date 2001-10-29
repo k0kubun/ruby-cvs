86fd3cf4b6ae8d2db243ed182d71890cab690b03
matz <>
2001-10-29 05:07:26 +0000

    * parse.y (str_extend): shuould allow interpolation of $-x.
    * variable.c (rb_cvar_set): empty iv_tbl may cause infinite loop.
    
    * variable.c (rb_cvar_get): ditto.
    
    * variable.c (cvar_override_check): ditto.
    
    * bignum.c (rb_big_eq): convert Bignum to Float, instead of
      reverse.
    
    * time.c (time_localtime): getting tm should not be prohibited for
      frozen time objects.
    
    * time.c (time_gmtime): ditto.
    
    * version.c (Init_version): freeze RUBY_VERSION,
      RUBY_RELEASE_DATE, and RUBY_PLATFORM.
    
    * file.c (Init_File): freeze File::SEPARATOR, ALT_SEPARATOR and
      PATH_SEPARATOR.
    
    * file.c (rb_stat_cmp): should check operand type before calling
      get_stat().
    
    * eval.c (rb_eval_cmd): should not invoke "call" with a block on
      any occasion.
    
    * numeric.c (fix_aref): idx may be a Bignum.
    
    * numeric.c (num_remainder): a bug in Numeric#remainder.
    
    * eval.c (rb_exec_end_proc): END might be called within END
      block.
    
    * class.c (rb_mod_clone): should not copy class name, since clone
      should remain anonymous.

--- ruby/lib/tracer.rb
+++ ruby/lib/tracer.rb
@@ -17,12 +17,12 @@ class Tracer
   @RCS_ID='-$Id$-'
 
   @stdout = STDOUT
+  @verbose = false
   class << self
     attr :verbose, true
     alias verbose? verbose
     attr :stdout, true
   end
-  verbose = true
   
   MY_FILE_NAME = caller(0)[0].scan(/^(.*):[0-9]+$/)[0][0]
   
@@ -155,7 +155,7 @@ class Tracer
   
 end
 
-LINES__ = {} unless defined? LINES__
+SCRIPT_LINES__ = {} unless defined? SCRIPT_LINES__
 
 if caller(0).size == 1
   if $0 == Tracer::MY_FILE_NAME
