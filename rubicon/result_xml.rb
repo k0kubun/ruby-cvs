fe823c213f855618a8edc84771ac1242a5253b86
dave <>
2001-11-09 06:06:12 +0000

    Map exception classes into strings.
    The class names aren;t transportable across systems.

--- rubicon/result_xml.rb
+++ rubicon/result_xml.rb
@@ -16,6 +16,14 @@ class ResultDisplay
   end
 
   def reportOn(op)
+    # map errors to the corresponding string - for some reason
+    # dump doesn't handle them
+
+    @gatherer.results.each_value do |res|
+      res.failures.each {|f| f.err = f.err.class.name + ": " + f.err.to_s }
+      res.errors.each   {|f| f.err = f.err.class.name + ": " + f.err.to_s }
+    end
+
     XMarshal.dump(@gatherer, op)
   end
 end
