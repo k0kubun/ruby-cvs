3bafdd9430dd44c930699f42bf0f80426031e056
shugo <>
2001-12-27 06:31:45 +0000

    * version 0.9.5 released.

--- mod_ruby/mod_ruby.h
+++ mod_ruby/mod_ruby.h
@@ -30,7 +30,7 @@
 #ifndef MOD_RUBY_H
 #define MOD_RUBY_H
 
-#define MOD_RUBY_STRING_VERSION "mod_ruby/0.9.4"
+#define MOD_RUBY_STRING_VERSION "mod_ruby/0.9.5"
 #define RUBY_GATEWAY_INTERFACE "CGI-Ruby/1.1"
 
 typedef struct {
