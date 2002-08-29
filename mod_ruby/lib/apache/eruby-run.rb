63396aa210ebf2b7a83beeadeec9e1b2ab62c83c
shugo <>
2002-08-29 04:35:23 +0000

    * lib/apache/eruby-run.rb (postrun): do not overwrite r.content_type.

--- mod_ruby/lib/apache/eruby-run.rb
+++ mod_ruby/lib/apache/eruby-run.rb
@@ -122,7 +122,9 @@ module Apache
         elsif r.sync_output or r.sync_header
           # Do nothing: header has already been sent
 	else
-	  r.content_type = format("text/html; charset=%s", ERuby.charset)
+	  unless r.content_type
+	    r.content_type = format("text/html; charset=%s", ERuby.charset)
+	  end
 	  r.send_http_header
 	end
       end
